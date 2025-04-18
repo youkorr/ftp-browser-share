6d4982d648790b05e5c96632a9e34647884b4752
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-18 03:33:07 +0200

    Update run.sh

--- my-ftp-browser/rootfs/run.sh
+++ my-ftp-browser/rootfs/run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/with-contenv bashio
 # Démarrer les services via S6
-/etc/services.d/nginx/run &
-/etc/services.d/ftp-server/run &
-
-# Attendre la fin des processus
-wait
+bashio::log.info "Starting FTP Browser Share services..."
