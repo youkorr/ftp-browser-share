04841b55484e69b863d13b9e78a7b0b8a2ef5121
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-18 06:41:23 +0200

    Update run.sh

--- my-ftp-browser/rootfs/run.sh
+++ my-ftp-browser/rootfs/run.sh
@@ -3,10 +3,10 @@
 
 # Vérifier et corriger les permissions
 chmod 755 /run.sh
-chmod 755 /my-ftp-browser/rootfs/etc/services.d/ftp_browser/run
-
+chmod 755 /etc/services.d/ftp_browser/run
 chmod 755 /etc/cont-init.d/*
 
 # Démarrer les services via S6
 echo "Démarrage des services FTP Browser Share..."
 exec /init
+
