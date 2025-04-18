e3d4d33e59da4ff4f0c467451d15bc610c28cd07
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-18 04:21:39 +0200

    Update run.sh

--- my-ftp-browser/run.sh
+++ my-ftp-browser/run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/with-contenv bashio
+# Script de démarrage principal
+
 # Démarrer Nginx
-nginx
+nginx &
 
 # Démarrer l'API Python
-python3 /usr/share/ftpbrowser/api/server.py
+python3 /usr/share/ftpbrowser/api/server.py &
+
+# Attendre la fin des processus
+wait
